c2401f32a87b70ae20fc5fc7addd84bacdd4cdce
Fs02 <surya.asriadie@gmail.com>
2016-02-20 02:11:03 +0700

    image crop with visual bug

--- app/assets/javascripts/controllers/items_ctrl.js
+++ app/assets/javascripts/controllers/items_ctrl.js
@@ -1,30 +1,37 @@
 angular.module('OpenData')
-.controller('ItemsCtrl',  [
-'$scope',
-'$stateParams',
-'Item',
-function($scope, $stateParams, Item) {
-  //the image
-  $scope.image;
+.controller('ItemsCtrl', [
+  '$scope',
+  '$stateParams',
+  'Item',
+  function($scope, $stateParams, Item) {
+    //the image
+    $scope.image;
+    $scope.displayHeight = 0;
+    $scope.displayWidth = 0;
+    $scope.scaledWidth = 0;
+    $scope.scaledHeight = 0;
+    $scope.scaledTop = 0;
+    $scope.scaledLeft = 0;
 
-  $scope.uploadImage = function() {
-    var fd = new FormData();
-    var imgBlob = dataURItoBlob($scope.image);
-    fd.append('file', imgBlob);
-    $http.post(
-      'imageURL',
-      fd, {
-        transformRequest: angular.identity,
-        headers: {
-          'Content-Type': undefined
-        }
-      }
-    )
-    .success(function(response) {
-      console.log('success', response);
-    })
-    .error(function(response) {
-      console.log('error', response);
-    });
+    $scope.uploadImage = function() {
+      var fd = new FormData();
+      var imgBlob = dataURItoBlob($scope.image);
+      fd.append('file', imgBlob);
+      $http.post(
+          'imageURL',
+          fd, {
+            transformRequest: angular.identity,
+            headers: {
+              'Content-Type': undefined
+            }
+          }
+        )
+        .success(function(response) {
+          console.log('success', response);
+        })
+        .error(function(response) {
+          console.log('error', response);
+        });
+    }
   }
-}]);
+]);
