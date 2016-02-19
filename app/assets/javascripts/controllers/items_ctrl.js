8a44400362e3633169dba13d39c6e68bffa056b3
Fs02 <surya.asriadie@gmail.com>
2016-02-20 01:16:20 +0700

    add uploadfile and fileread directives

--- app/assets/javascripts/controllers/items_ctrl.js
+++ app/assets/javascripts/controllers/items_ctrl.js
@@ -4,7 +4,27 @@ angular.module('OpenData')
 '$stateParams',
 'Item',
 function($scope, $stateParams, Item) {
-  $scope.create = function(item) {
+  //the image
+  $scope.image;
 
+  $scope.uploadImage = function() {
+    var fd = new FormData();
+    var imgBlob = dataURItoBlob($scope.image);
+    fd.append('file', imgBlob);
+    $http.post(
+      'imageURL',
+      fd, {
+        transformRequest: angular.identity,
+        headers: {
+          'Content-Type': undefined
+        }
+      }
+    )
+    .success(function(response) {
+      console.log('success', response);
+    })
+    .error(function(response) {
+      console.log('error', response);
+    });
   }
 }]);
---
+++ app/assets/javascripts/directives/fileread.js
@@ -0,0 +1,22 @@
+// http://stackoverflow.com/a/32011326
+angular.module('OpenData')
+.directive("fileread", [
+  function() {
+    return {
+      scope: {
+        fileread: "="
+      },
+      link: function(scope, element, attributes) {
+        element.bind("change", function(changeEvent) {
+          var reader = new FileReader();
+          reader.onload = function(loadEvent) {
+            scope.$apply(function() {
+              scope.fileread = loadEvent.target.result;
+            });
+          }
+          reader.readAsDataURL(changeEvent.target.files[0]);
+        });
+      }
+    }
+  }
+]);
