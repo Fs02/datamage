9101da801c2bff836a08c91487ef0e38cb131424
Fs02 <surya.asriadie@gmail.com>
2016-02-20 00:32:47 +0700

    some refactoring and initial item new page

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -7,9 +7,14 @@ function($stateProvider, $urlRouterProvider) {
   $stateProvider
     .state('explores', {
       url: '/explores/:category_id',
-      templateUrl: 'views/explores/_gallery.html',
+      templateUrl: 'views/explores/gallery.html',
       controller: 'ExploresCtrl'
-    });
+    })
+    .state('items_new', {
+        url: '/items/new',
+        templateUrl: 'views/items/new.html',
+        controller: 'ItemsCtrl'
+      });
 
   $urlRouterProvider.otherwise('explores');
 }]);
---
+++ app/assets/javascripts/controllers/items_ctrl.js
@@ -0,0 +1,10 @@
+angular.module('OpenData')
+.controller('ItemsCtrl',  [
+'$scope',
+'$stateParams',
+'Item',
+function($scope, $stateParams, Item) {
+  $scope.create = function(item) {
+
+  }
+}]);
