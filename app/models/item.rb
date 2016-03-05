b49b3dee2d23675829c4b4dd8ac779b828db7201
Fs02 <surya.asriadie@gmail.com>
2016-03-06 01:57:51 +0700

    adjustement and improved slack_notifier and item model

--- app/models/item.rb
+++ app/models/item.rb
@@ -26,7 +26,22 @@ class Item
 
   validates_with LeafCategoryValidator
 
-  after_save :detect_duplicates
+  after_save do
+    self.delay.notify_uploads
+    self.delay.detect_duplicates
+  end
+
+  def notify_uploads
+    attachments = [
+        {
+            fallback: "[INFO] New Upload #{self.id.to_s}",
+            title: "[INFO] New Upload #{self.id.to_s}",
+            image_url: "http://datamage.local/api/items/#{self.id.to_s}/image",
+            color: "good"
+        }
+    ]
+    SlackNotifier.ping(attachments: attachments, channel: '#activity')
+  end
 
   def detect_duplicates
     filename = self.image.model.read_attribute(:image)
@@ -45,7 +60,7 @@ class Item
     batch_iter = 0
     batch_size = 100
     duplicates = []
-    while (items = Item.limit(batch_size).skip(batch_iter * batch_size).pluck(:id, :phash).to_a).present?
+    while (items = self.category.items.limit(batch_size).skip(batch_iter * batch_size).pluck(:id, :phash).to_a).present?
       items.each do |i|
         if i[0].to_s != self.id.to_s and Phashion.hamming_distance(self.phash.to_i, i[1].to_i) < 15
           duplicates += [i[0]]
@@ -56,8 +71,18 @@ class Item
     end
 
     self.set(duplicates: duplicates)
-#    self.save!
+
     Rails.logger.info "[Detect Duplicates (#{self.id.to_s})] Found #{self.duplicates.count} duplicates"
+    attachments = [
+        {
+            fallback: "[ALERT] Found #{self.duplicates.count} duplicates",
+            title: "#{self.duplicates.count} duplicates detected on #{self.id.to_s}",
+            text: "Duplicates :  #{self.duplicates.map { |d| d.to_s }.join(' , ')}",
+            image_url: "http://datamage.local/api/items/#{self.id.to_s}/image",
+            color: "danger"
+        }
+    ]
+    SlackNotifier.ping(attachments: attachments, channel: '#duplicates') unless duplicates.empty?
   end
 
 end
